6d30d61b0b5d719a78c783f428ce2987556e2777
Vinayak Kaushik <vinayakkaushik@protonmail.com>
2023-06-20 00:22:56 +0530

    Modifies image type

--- profiledef.sh
+++ profiledef.sh
@@ -13,7 +13,7 @@ bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
            'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
-airootfs_image_type="squashfs"
+airootfs_image_type="ext4+squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
